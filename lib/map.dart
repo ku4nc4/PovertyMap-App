ea822de6cc6b7536b260d0d14cffa6db00dc4bb3
singgih <singgihsurya91@gmail.com>
2019-02-09 18:17:35 +0700

    map ok

--- lib/map.dart
+++ lib/map.dart
@@ -1,39 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-//class MapView extends StatefulWidget {
-//  @override
-//  _MapViewState createState() => _MapViewState();
-//}
-//
-//class _MapViewState extends State<MapView> {
-//  GoogleMapController mapController;
-//
-//  final LatLng _center = const LatLng(45.521563, -122.677433);
-//
-//  void _onMapCreated(GoogleMapController controller) {
-//    mapController = controller;
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('Maps Sample App'),
-//          backgroundColor: Colors.green[700],
-//        ),
-//        body: GoogleMap(
-//          onMapCreated: _onMapCreated,
-//          initialCameraPosition: CameraPosition(
-//              target: _center,
-//              zoom: 11.0,
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(-6.2717416,106.5642999);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class MapsDemo extends StatefulWidget {
   @override
@@ -47,33 +47,34 @@ class MapsDemoState extends State<MapsDemo> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(15.0),
+      padding: EdgeInsets.all(24.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Center(
             child: SizedBox(
               width: 300.0,
-              height: 200.0,
+              height: 500.0,
               child: GoogleMap(
                 onMapCreated: _onMapCreated,
-                initialCameraPosition: new CameraPosition(target: new LatLng(45.521563, -122.677433)),
+                initialCameraPosition: new CameraPosition(target: new LatLng(-6.2717416,106.5642999)),
               ),
             ),
           ),
-          RaisedButton(
-            child: const Text('Go to London'),
-            onPressed: mapController == null ? null : () {
-              mapController.animateCamera(CameraUpdate.newCameraPosition(
-                const CameraPosition(
-                  bearing: 270.0,
-                  target: LatLng(51.5160895, -0.1294527),
-                  tilt: 30.0,
-                  zoom: 17.0,
-                ),
-              ));
-            },
-          ),
+//          RaisedButton(
+//            child: const Text('Go to London'),
+//            onPressed: mapController == null ? null : () {
+//              mapController.animateCamera(CameraUpdate.newCameraPosition(
+//                const CameraPosition(
+//                  bearing: 270.0,
+//                  target: LatLng(51.5160895, -0.1294527),
+//                  tilt: 30.0,
+//                  zoom: 17.0,
+//                ),
+//              ));
+//            },
+//          ),
+//
         ],
       ),
     );
